7fa6be5cb91a0164382cec6718b96300d7f30c9a
alexandrer0 <alexandrer@yandex.ru>
2019-07-01 00:45:08 +0300

    преобразовать amount в число

--- vtb.py
+++ vtb.py
@@ -1,6 +1,7 @@
 import pandas as pd
 df = pd.read_csv('details.csv', encoding='ANSI', sep=';', skiprows=11)
 
+
 df.drop(columns=['Дата обработки',
                  'Валюта операции',
                  'Номер карты/счета/договора',
@@ -16,13 +17,22 @@ df.rename(columns={'Дата операции': 'transaction_date',
 df['amount'] = [x.replace(',', '.') for x in df['amount']]
 df.query("status == 'Исполнено'", inplace=True)
 df['transaction_date'] = df['transaction_date'].apply(pd.to_datetime)
-pd.to_numeric(df['amount'], errors='ignore')
+# pd.to_numeric(df['amount'], errors='ignore')
+# df['amount'].apply(pd.to_numeric())
+# df.amount = pd.to_numeric(df.amount, errors='ignore')
 
 df = df[df['cause'].str.startswith('PYATEROCHKA', na=False)]
-df0619 = df.query("transaction_date >= '2019-06-01'")
+df19 = df.query("transaction_date >= '2019-01-01'")
+df19['month'] = df19['transaction_date'].dt.strftime('%m%Y')
+df19.drop(columns=['transaction_date',
+                   'cause',
+                   'status'],
+          inplace=True)
+df19mn = df19.groupby('month')['amount'].sum()
+# df19.DataDoc.dt.
 # df = df[df['transaction_date'].date()]
-print(df0619)
-print(df0619.dtypes)
-print(df0619.shape)
+print(df19mn)
+print(df19.dtypes)
+print(df19.shape)
 
 # df0619.to_excel('det.xlsx', header = True, index = False)
